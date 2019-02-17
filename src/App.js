f028889b6b396cff762fa7931478f08341cc2b96
manan999 <aggarwalmanan4@gmail.com>
2019-02-17 16:35:45 +0530

    added welcome

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom' ;
+
 import './App.css';
+import Welcome from './comps/Welcome.js'
 
 class App extends Component {
+ 
   render() {
     return (
       <div className="App">
-          This is my app
+          <BrowserRouter>
+            <div>
+              <Route path='/' exact component={Welcome} />
+                          </div>
+          </BrowserRouter>
       </div>
     );
   }
 }
 
 export default App;
+
+// <Route path='/test/stress-level' component={StressLevelTest} />
+//               <Route path='/test/other' component={StressOther} />
+//               <Route path='/login' component={Login} />
+//               <Route path='/register' component={Register} />
+//               <Route path='/tips' component={Tips} />
---
+++ src/comps/Welcome.js
@@ -0,0 +1,16 @@
+import React from 'react' ;
+
+const Welcome = () => {
+	return (
+		<div>
+			<h1> Welcome to Stress-No-More </h1>
+			<div>
+				<div> Login </div>
+				<div> Tips </div>
+				<div> Test </div>
+			</div> 
+		</div>
+		) ;
+}
+
+export default Welcome ;
